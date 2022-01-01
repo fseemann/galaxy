52ec11ceb51c59767c01871f002304dd22318fff
Felix Seemann <fseemann@mail.de>
2022-01-01 19:30:59 +0100

    remove comment

--- galaxy-backend/src/main/kotlin/com/manic/galaxy/infrastructure/ktor/Application.kt
+++ galaxy-backend/src/main/kotlin/com/manic/galaxy/infrastructure/ktor/Application.kt
@@ -3,8 +3,7 @@ package com.manic.galaxy.infrastructure.ktor
 import com.manic.galaxy.infrastructure.ktor.plugins.*
 import io.ktor.application.*
 
-@Suppress(
-    "unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+@Suppress("unused")
 fun Application.module() {
     configureDependencyInjection()
     configureRouting()
