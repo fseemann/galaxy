8f404c2396879e7318edd03dc3d5641e6918ac2d
Felix Seemann <fseemann@mail.de>
2022-01-01 19:31:55 +0100

    Fix module

--- galaxy-backend/src/main/kotlin/com/manic/galaxy/infrastructure/koin/ModuleFactory.kt
+++ galaxy-backend/src/main/kotlin/com/manic/galaxy/infrastructure/koin/ModuleFactory.kt
@@ -2,6 +2,7 @@ package com.manic.galaxy.infrastructure.koin
 
 import com.manic.galaxy.application.FacilityService
 import com.manic.galaxy.application.GalaxyService
+import com.manic.galaxy.application.UserService
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
@@ -10,7 +11,7 @@ object ModuleFactory {
         return module {
             single { FacilityService(get()) }
             single { GalaxyService(get(), get(), get()) }
-            single { FacilityService(get()) }
+            single { UserService(get(), get(), get(), get()) }
         }
     }
 }
