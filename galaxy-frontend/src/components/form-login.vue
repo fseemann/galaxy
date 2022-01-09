ec134a904c3c5b4ec27a923c633fe22a4af6a050
Felix Seemann <fseemann@mail.de>
2022-01-09 12:50:27 +0100

    Add a login component

---
+++ galaxy-frontend/src/components/form-login.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+<div>
+  <h2>Login</h2>
+  <form>
+    <div>
+      <label for="username">Username</label>
+      <input id="username" name="username" type="text" />
+      <span>Enter your email.</span>
+    </div>
+    <div>
+      <label for="password">Password</label>
+      <input id="password" name="password" type="password" />
+    </div>
+  </form>
+</div>
+</template>
