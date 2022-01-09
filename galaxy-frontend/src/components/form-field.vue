a79f78bb76f02c56d54eb041c557a98e1ba7fa68
Felix Seemann <fseemann@mail.de>
2022-01-09 13:02:02 +0100

    Display login form

---
+++ galaxy-frontend/src/components/form-field.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <label :for="inputId">{{label}}</label>
+  <slot :id="inputId" :aria-describedby="descriptionId" />
+  <span :id="descriptionId">{{description}}</span>
+</div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+defineProps({
+  label: String,
+  description: String
+})
+
+const inputId = computed(() => Math.random().toString(36).substring(1, 6))
+const descriptionId = computed(() => Math.random().toString(36).substring(1, 6))
+</script>
+
+<style scoped>
+
+</style>
--- galaxy-frontend/src/components/form-login.vue
+++ galaxy-frontend/src/components/form-login.vue
@@ -1,20 +1,17 @@
-<script setup>
-
-</script>
-
 <template>
 <div>
   <h2>Login</h2>
   <form>
-    <div>
-      <label for="username">Username</label>
-      <input id="username" name="username" type="text" />
-      <span>Enter your email.</span>
-    </div>
-    <div>
-      <label for="password">Password</label>
-      <input id="password" name="password" type="password" />
-    </div>
+    <form-field label="Username" description="Enter your email." v-slot="props">
+      <input v-bind="props" id="username" name="username" type="text" />
+    </form-field>
+    <form-field label="Password" v-slot="props">
+      <input v-bind="props" id="password" name="password" type="password" />
+    </form-field>
+    <button>Submit</button>
   </form>
 </div>
 </template>
+
+<script setup>
+import FormField from './form-field.vue'</script>
